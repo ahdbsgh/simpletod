a0ce21d944ec7699557f09414d70f19f888c1365
momozzing <ahdbsgh2@naver.com>
2021-05-04 16:23:12 +0900

    study

--- train_end2end.sh
+++ train_end2end.sh
@@ -1,16 +1,16 @@
 
 
 
-export MODEL=$2
-export MODEL_NAME=$3
-export BATCH=$4
+export MODEL='gpt2'
+export MODEL_NAME='gpt2-large'
+# export BATCH=$4
 export OUTPUT=output/${MODEL_NAME}
 
 export TRAIN_FILE=./resources/gpt2/train.history_belief_action_sys_delex
 export TEST_FILE=./resources/gpt2/val.history_belief_action_sys_delex
 
 
-CUDA_VISIBLE_DEVICES=$1 python main.py \
+CUDA_VISIBLE_DEVICES=1 python main.py \
     --output_dir=$OUTPUT \
     --model_type=$MODEL \
     --model_name_or_path=$MODEL_NAME \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=$1 python main.py \
     --do_eval \
     --eval_data_file=$TEST_FILE \
     --evaluate_during_training \
-    --save_steps 10000 \
-    --logging_steps 1000 \
-    --per_gpu_train_batch_size $BATCH \
-    --num_train_epochs 100
+    --save_steps 1 \
+    --logging_steps 1 \
+    --num_train_epochs 1
+    #--per_gpu_train_batch_size $BATCH \
